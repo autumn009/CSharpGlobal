17920bcc2ede42761ad81ed41d2a370f8080cbec
Akira Kawamata <autumn@piedey.co.jp>
2023-12-10 19:56:50 +0900

    add Sample002

---
+++ Sample002/Sample002/Program.cs
@@ -0,0 +1,6 @@
+﻿var dt1 = new DateTime(2024, 2, 29);
+for (int i = 0; i < 4; i++) dt1 = dt1.AddYears(1);  // 1年を4回加える
+Console.WriteLine(dt1.ToShortDateString());
+var dt2 = new DateTime(2024, 2, 29);
+dt2 = dt2.AddYears(4);  // 4年を1回加える
+Console.WriteLine(dt2.ToShortDateString());
