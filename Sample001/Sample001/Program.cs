6331c8b5894d5bfabe9d86ac19e9984a276cfa97
Akira Kawamata <autumn@piedey.co.jp>
2023-12-10 19:04:27 +0900

    addSample001

---
+++ Sample001/Sample001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+var dt = new DateTime(2029, 6, 2);
+
+sub(dt);
+Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+sub(dt);
+Thread.CurrentThread.CurrentCulture = new CultureInfo("ja-JP");
+sub(dt);
+
+
+void sub(DateTime dt)
+{
+    Console.WriteLine($"{120:C}");
+    Console.WriteLine(dt);
+}
