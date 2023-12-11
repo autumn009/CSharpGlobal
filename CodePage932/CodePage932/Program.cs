3fce37a32667b0fe95dadc9e7c820dd87cad5844
Akira Kawamata <autumn@piedey.co.jp>
2023-12-12 08:09:46 +0900

    add CodePage932

---
+++ CodePage932/CodePage932/Program.cs
@@ -0,0 +1,11 @@
+﻿sub();
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+sub();
+Console.OutputEncoding = System.Text.Encoding.GetEncoding(932);
+sub();
+
+void sub()
+{
+    Console.WriteLine($"Encoding: {Console.OutputEncoding.EncodingName}");
+    Console.WriteLine($"Price {120:C}");
+}
