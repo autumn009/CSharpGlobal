fe18cdafaab24b586c542ab610dca5a1fc09ed68
Akira Kawamata <autumn@piedey.co.jp>
2023-12-11 17:06:03 +0900

    add NonNumberDate

---
+++ NonNumberDate/NonNumberDate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+var dt = new DateTime(2029, 6, 2);
+Console.WriteLine("アメリカの英語");
+Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+Console.WriteLine(dt.ToString("yyyy MMMM dd"));
+Console.WriteLine("日本の日本語");
+Thread.CurrentThread.CurrentCulture = new CultureInfo("ja-JP");
+Console.WriteLine(dt.ToString("yyyy MMMM dd"));
+Console.WriteLine("インドのヒンディー語");
+Thread.CurrentThread.CurrentCulture = new CultureInfo("hi-IN");
+Console.WriteLine(dt.ToString("yyyy MMMM dd"));
