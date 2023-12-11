debb55e72b2cc2b3f16da7351a352197a4b7cdc7
Akira Kawamata <autumn@piedey.co.jp>
2023-12-11 11:17:29 +0900

    add SetCulture

---
+++ SetCulture/SetCulture/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+var dt = new DateTime(2029, 6, 2);
+
+sub(dt);
+Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+sub(dt);
+Thread.CurrentThread.CurrentCulture = new CultureInfo("ja-JP");
+sub(dt);
+
+static void sub(DateTime dt) => Console.WriteLine($"{Thread.CurrentThread.CurrentCulture.Name} {dt.ToShortDateString()}");
