b664e0b6c7b08e7924c9a01d479b4c27e533f711
Akira Kawamata <autumn@piedey.co.jp>
2023-12-12 09:38:46 +0900

    add Encodings

---
+++ Encodings/Encodings/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+var e1 = Encoding.UTF8;
+Console.WriteLine(e1.EncodingName);
+var e2 = Encoding.GetEncoding(932);
+Console.WriteLine(e2.EncodingName);
+var e3 = Encoding.GetEncoding("Shift_JIS");
+Console.WriteLine(e3.EncodingName);
