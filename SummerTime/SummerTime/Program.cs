1fb7cf1a34b3bdc5f8d71c2d5ba23dc711fed556
Akira Kawamata <autumn@piedey.co.jp>
2023-12-13 15:34:17 +0900

    add SummerTime

---
+++ SummerTime/SummerTime/Program.cs
@@ -0,0 +1,9 @@
+﻿DateTime targetDate1 = new DateTime(2029, 3, 1);
+DateTimeOffset dateAndOffset1 = new DateTimeOffset(targetDate1,
+                         TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time").GetUtcOffset(targetDate1));
+
+DateTime targetDate2 = new DateTime(2029, 4, 1);
+DateTimeOffset dateAndOffset2 = new DateTimeOffset(targetDate2,
+                         TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time").GetUtcOffset(targetDate2));
+
+Console.WriteLine($"[{targetDate2}]-[{targetDate1}]=[{dateAndOffset2-dateAndOffset1}]");
