190c1465135373682a05b582bdca9941fb4a9579
Akira Kawamata <autumn@piedey.co.jp>
2023-12-13 16:18:28 +0900

    add LongShortCustom

---
+++ LongShortCustom/LongShortCustom/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+var dt = new DateTime(2029, 6, 2);
+
+Thread.CurrentThread.CurrentCulture = new CultureInfo("ja-JP");
+sub(dt);
+Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+sub(dt);
+Thread.CurrentThread.CurrentCulture = new CultureInfo("fr-FR");
+sub(dt);
+Thread.CurrentThread.CurrentCulture = new CultureInfo("hi-IN");
+sub(dt);
+
+void sub(DateTime dt)
+{
+    Console.WriteLine($"NativeName={Thread.CurrentThread.CurrentCulture.NativeName} Name={Thread.CurrentThread.CurrentCulture.Name}");
+    Console.WriteLine($"LongDateString={dt.ToLongDateString()}");
+    Console.WriteLine($"ShortDateString={dt.ToShortDateString()}");
+    Console.WriteLine($"CustomDateString(yy yyyy MM MMM dd ddd)={dt.ToString("yy yyyy MM MMM dd ddd")}");
+}
